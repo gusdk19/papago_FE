f2860536f94a24dc37192d1286c84dc64077b652
gusdk19 <hyuna030619@naver.com>
2024-07-11 20:02:44 +0900

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+    content: [
+      "./index.html",
+    ],
+    darkMode: 'class',
+    theme: {
+      extend: {
+        fontFamily: {
+          nanum: ['Nanum Gothic']
+        }
+      }
+    }
+  }
